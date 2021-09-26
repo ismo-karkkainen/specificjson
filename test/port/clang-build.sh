10a34ec453d11aebf05cfa61e20ebffa0a7b8fa6
ismo-karkkainen <3896372+ismo-karkkainen@users.noreply.github.com>
2021-09-26 21:56:58 +0300

    Print out required own gems for diagram data gathering.

--- test/port/clang-build.sh
+++ test/port/clang-build.sh
@@ -2,6 +2,7 @@
 
 set -eu
 
+echo 'Require edicta'
 gem install edicta
 CXX=clang++ cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=DEBUG $1
 make -j 3
--- test/port/gcc-build.sh
+++ test/port/gcc-build.sh
@@ -2,6 +2,7 @@
 
 set -eu
 
+echo 'Require edicta'
 gem install edicta
 cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=DEBUG $1
 make -j 3
