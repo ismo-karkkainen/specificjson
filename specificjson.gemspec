aecedbe6333d040ef42575465ddbab7410180a90
ismo-karkkainen <3896372+ismo-karkkainen@users.noreply.github.com>
2021-12-30 18:43:54 +0200

    Use safe_load and allow aliases.

--- specificjson.gemspec
+++ specificjson.gemspec
@@ -1,7 +1,7 @@
 Gem::Specification.new do |s|
   s.name        = 'specificjson'
-  s.version     = '0.1.0'
-  s.date        = '2021-09-16'
+  s.version     = '0.1.1'
+  s.date        = '2021-12-30'
   s.summary     = 'Generate C++ JSON-parsing source from input description.'
   s.description = %q(
 A tool to generate C++ header and source file with templates for parsing and
@@ -15,6 +15,6 @@ Licensed under Universal Permissive License, see LICENSE.txt.
   s.email       = 'ismokarkkainen@icloud.com'
   s.files       = [ 'LICENSE.txt' ]
   s.executables << 'specificjson'
-  s.homepage    = 'http://xn--ismo-krkkinen-gfbd.fi/specificjson/index.html'
+  s.homepage    = 'https://xn--ismo-krkkinen-gfbd.fi/specificjson/index.html'
   s.license     = 'UPL-1.0'
 end
--- test/cloudbuild.sh
+++ test/cloudbuild.sh
@@ -4,24 +4,44 @@ set -u
 export D=$1
 R=$2
 
-C="gem install edicta"
-echo "$C" && $C
-cd $R
+export C="gem install edicta"
+$C
 
+# Scripts from installed gems not found (in OpenSUSE Tumbleweed)?
+for X in edicta
+do
+    $X --help >/dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        continue
+    fi
+    for P in /usr/lib64/ruby/gems
+    do
+        S=$(find $P -type f -name $X -perm -0555)
+        if [ $? -eq 0 ]; then
+            export PATH="$PATH:$(dirname $S)"
+            echo "Found $X in $S"
+            break
+        fi
+        echo "$X not found under $P"
+    done
+done
+
+cd $R
 for X in clang++ g++
 do
     export X
-    mkdir build
+    mkdir testbuild
     (
         echo "Build $(cat _logs/commit.txt) on $D using $X at $(date '+%Y-%m-%d %H:%M')"
+        echo "$C"
         (
             set -eu
-            cd build
+            cd testbuild
             CXX=$X cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release ..
             make -j 2
             make test
         )
         echo "Build and test exit code: $?"
     ) 2>&1 | tee -a "$R/_logs/$D-$X.log"
-    rm -rf build
+    rm -rf testbuild
 done
